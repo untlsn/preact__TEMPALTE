09b15ae1b37af211f6aeb0cac8f8467331773017
Filip Skoczen <philipskoczen@gmail.com>
2022-09-21 16:54:26 +0200

    init

---
+++ src/app.tsx
@@ -0,0 +1,34 @@
+import preactLogo from './assets/preact.svg'
+import './app.css'
+import { useSignal } from '@preact/signals';
+
+export function App() {
+  const count = useSignal(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://preactjs.com" target="_blank">
+          <img src={preactLogo} class="logo preact" alt="Preact logo" />
+        </a>
+      </div>
+      <h1>Vite + Preact</h1>
+      <div class="card">
+        <button onClick={() => {
+          count.value++
+        }}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/app.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p class="read-the-docs">
+        Click on the Vite and Preact logos to learn more
+      </p>
+    </>
+  )
+}
